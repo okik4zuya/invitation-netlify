9b7c7cf1f5b0b2bf384bb2948b99ff8cfef6b104
okik4zuya <okik4zuya@gmail.com>
2022-01-26 11:28:45 +0700

    main

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,23 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from 'next/head';
+import Header from '@components/Header';
+import Footer from '@components/Footer';
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className='container'>
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Next.js Starter! I'm trying</title>
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
+        <Header title='Welcome to my app!' />
+        <p className='description'>
           Get started by editing <code>pages/index.js</code>
         </p>
       </main>
 
       <Footer />
     </div>
-  )
+  );
 }
